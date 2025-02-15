6118c1428b381c41119a970d48e654893b1abfc0
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-02-15 06:28:19 -0300

    add an abstract class to serve as a basis for fruit and other products that can be added in the future

---
+++ src/entities/Produto.java
@@ -0,0 +1,46 @@
+package entities;
+
+import interfaces.Vendavel;
+
+public abstract class Produto implements Vendavel {
+    private String nome;
+    private double precoUnitario;
+    private int quantidade;
+
+    public Produto(String nome, double precoUnitario, int quantidade) {
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + getNome() +
+                ", Preço: R$" + String.format("%.2f", precoUnitario) +
+                ", Quantidade: " + getQuantidade();
+    }
+}
